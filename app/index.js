3fdfdef5b50374da5e2ff4fe5861572a13946c0a
NfcUser <misu65339@gmail.com>
2026-06-03 18:52:11 +0300

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+export default function App() {
+  return (
+    <View style={{flex:1,backgroundColor:'#03060A',alignItems:'center',justifyContent:'center'}}>
+      <Text style={{color:'#00D4FF',fontSize:24,fontFamily:'monospace'}}>NFC TUNELESS</Text>
+    </View>
+  );
+}
