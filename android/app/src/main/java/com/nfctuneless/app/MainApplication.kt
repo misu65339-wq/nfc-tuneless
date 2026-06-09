2aff6d62680eaa954cb600f2a7243069e8db5900
NfcUser <misu65339@gmail.com>
2026-06-09 13:19:44 +0300

    Clean prebuild - fix duplicate HCE service v7.8

--- android/app/src/main/java/com/nfctuneless/app/MainApplication.kt
+++ android/app/src/main/java/com/nfctuneless/app/MainApplication.kt
@@ -5,7 +5,6 @@ import android.content.res.Configuration
 import androidx.annotation.NonNull
 
 import com.facebook.react.PackageList
-import com.nfctuneless.HcePackage
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
@@ -28,9 +27,7 @@ class MainApplication : Application(), ReactApplication {
           override fun getPackages(): List<ReactPackage> {
             // Packages that cannot be autolinked yet can be added manually here, for example:
             // packages.add(new MyReactNativePackage());
-            val packages = PackageList(this).packages
-            packages.add(HcePackage())
-            return packages
+            return PackageList(this).packages
           }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
