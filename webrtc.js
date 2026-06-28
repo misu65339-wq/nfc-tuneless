49b8f9e447e18d94d53c047ad86478647f97ea14
NfcUser <misu65339@gmail.com>
2026-06-28 18:07:31 +0300

    Use binary WebRTC APDU messages

--- webrtc.js
+++ webrtc.js
@@ -53,6 +53,7 @@ export default class WebRTCClient {
   }
 
   setupChannel() {
+    try{this.channel.binaryType="arraybuffer";}catch(e){}
     this.channel.onopen = () => {
       console.log("DataChannel OPEN");
       this.onStatus("WEBRTC_OPEN");
